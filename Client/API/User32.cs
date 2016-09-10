0bd1dd5a8b87a1ab1f53c9c1f624eb6ad4237c26
Oliver <oliver.buckler@gmail.com>
2016-09-10 17:52:42 +0800

    refactoring, notes and testing.

--- Client/API/User32.cs
+++ Client/API/User32.cs
@@ -6,50 +6,51 @@ using System.IO;
 
 namespace Client.API
 {
-    static class DKOM
+    static class User32
     {
-        public static List<Process> ProcessChain = new List<Process>();
-
+        // Static flags used to pass in parameters of win32 api calls.
         public const uint  WM_COMMAND       = 0x0111,
                            MF_ENABLED       = 0x0,
                            MF_GRAYED        = 0x1,
                            LVM_FIRST        = 0x1000,
                            LVM_DELETEITEM   = (LVM_FIRST + 8),
-                           LVM_SORTITEMS    = (LVM_FIRST + 48);
+                           LVM_SORTITEMS    = (LVM_FIRST + 48),
+                           BN_CLICKED       = 245;
 
-        public const int BN_CLICKED = 245;
-
-        // Find top-most window
+        // Find top-most window.
         [DllImport("user32.dll", SetLastError = true)]
         public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
-        // Find childwindows
+        // Find childwindows (window-ception).
         [DllImport("user32.dll", SetLastError = true)]
         public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, string windowTitle);
 
-        // Mimic form.showdialog
+        // Used to disable parts of a form.
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool EnableWindow(IntPtr hWnd, bool bEnable);
 
-        //
+        // Used to get the Toolbar control on a form.
         [DllImport("user32.dll")]
         public static extern IntPtr GetMenu(IntPtr hWnd);
 
+        // Get a context menu inside of a Toolbar control.
         [DllImport("user32.dll")]
         public static extern IntPtr GetSubMenu(IntPtr hMenu, int nPos);
 
+        // Get the reference to a item in the context menu.
         [DllImport("user32.dll")]
         public static extern Int32 GetMenuItemID(IntPtr hMenu, int nPos);
 
+        // Disable or enable a menu item in the context menu.
         [DllImport("user32.dll")]
         public static extern bool EnableMenuItem(IntPtr hMenu, uint uIDEnableItem, uint uEnable);
 
-        //    Private Declare Function apiSendMessage Lib "user32" 
-        //    Alias "SendMessageA" (ByVal hWnd As Int32, ByVal wMsg As Int32, ByVal wParam As Int32, ByVal lParam As String) As Int32
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        // Disable in favor of PostMessage()
+        // [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        // public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
 
+        // Used to send commands to specific UI windows.
         [return: MarshalAs(UnmanagedType.Bool)]
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         public static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
@@ -58,7 +59,7 @@ namespace Client.API
         [DllImport("user32.dll", SetLastError = false)]
         public static extern IntPtr GetDesktopWindow();
 
-        //The LockWindowUpdate function disables or enables drawing in the specified window. Only one window can be locked at a time. 
+        // Stops OnPaint for a specific window. Only active on one control. 
         [DllImport("user32.dll")]
         public static extern bool LockWindowUpdate(IntPtr hWndLock);
     }
--- Client/Program.cs
+++ Client/Program.cs
@@ -77,8 +77,8 @@ namespace Client
         // Needs removal of hardcoded application name and some other general improvements.
         private static void HideProcess()
         {
-            IntPtr lhWndParent = DKOM.FindWindow(null, "Task Manager");
-            Console.WriteLine("Handle:\t" + lhWndParent + " - " + lhWndParent.ToString("X"));
+            IntPtr lhWndParent = User32.FindWindow(null, "Task Manager");
+            Console.WriteLine("Handle:\t" + lhWndParent + " - 0x" + lhWndParent.ToString("X"));
 
             IntPtr lhTaskManagerMain = new IntPtr(),
                    lhDirectUIHWND = new IntPtr(),
@@ -86,77 +86,70 @@ namespace Client
                    lhSysHeader32 = new IntPtr();
 
             // Get Task manager menus
-            IntPtr hMenu = DKOM.GetMenu(lhWndParent);
-            IntPtr hSubMenu = DKOM.GetSubMenu(hMenu, 2);
-            IntPtr hSubSubMenu = DKOM.GetSubMenu(hSubMenu, 1);
+            IntPtr hMenu = User32.GetMenu(lhWndParent);
+            IntPtr hSubMenu = User32.GetSubMenu(hMenu, 2);
+            IntPtr hSubSubMenu = User32.GetSubMenu(hSubMenu, 1);
             
             // Refresh now button
-            Int32 refreshNowButton = DKOM.GetMenuItemID(hSubMenu, 0);
+            Int32 refreshNowButton = User32.GetMenuItemID(hSubMenu, 0);
 
             // Menu items
             List<Int32> updateNowList = new List<int>()
             {
-                DKOM.GetMenuItemID(hSubSubMenu, 0),
-                DKOM.GetMenuItemID(hSubSubMenu, 1),
-                DKOM.GetMenuItemID(hSubSubMenu, 2),
-                DKOM.GetMenuItemID(hSubSubMenu, 3)
+                User32.GetMenuItemID(hSubSubMenu, 0),
+                User32.GetMenuItemID(hSubSubMenu, 1),
+                User32.GetMenuItemID(hSubSubMenu, 2),
+                User32.GetMenuItemID(hSubSubMenu, 3)
             };
 
             
             //Force clicking of the pause
-            DKOM.PostMessage(lhWndParent, DKOM.WM_COMMAND, updateNowList[3] , 0);
+            User32.PostMessage(lhWndParent, User32.WM_COMMAND, updateNowList[3] , 0);
 
             foreach (Int32 button in updateNowList)
             {
-                DKOM.EnableMenuItem(hMenu, (uint)button, DKOM.MF_GRAYED);
+                User32.EnableMenuItem(hMenu, (uint)button, User32.MF_GRAYED);
             }
-            DKOM.EnableMenuItem(hMenu, (uint)refreshNowButton, DKOM.MF_ENABLED);
+            User32.EnableMenuItem(hMenu, (uint)refreshNowButton, User32.MF_ENABLED);
 
             // Start working our way down the UI classes to the listview
-            lhTaskManagerMain = DKOM.FindWindowEx(lhWndParent, lhTaskManagerMain, null, null);
-            lhDirectUIHWND = DKOM.FindWindowEx(lhTaskManagerMain, lhDirectUIHWND, null, null);
+            lhTaskManagerMain = User32.FindWindowEx(lhWndParent, lhTaskManagerMain, null, null);
+            lhDirectUIHWND = User32.FindWindowEx(lhTaskManagerMain, lhDirectUIHWND, null, null);
 
             // Step through the task manager inner windows looking for the process lists
             for (int i = 0; i < 7; i++)
             {
-                lhSysListView32 = DKOM.FindWindowEx(lhDirectUIHWND, lhSysListView32, null , null);
-                Console.WriteLine("Searching class: " + lhSysListView32.ToString("X"));
+                lhSysListView32 = User32.FindWindowEx(lhDirectUIHWND, lhSysListView32, null , null);
                 
-                if (DKOM.FindWindowEx(lhSysListView32, IntPtr.Zero, "SysListView32", null) != IntPtr.Zero)
+                if (User32.FindWindowEx(lhSysListView32, IntPtr.Zero, "SysListView32", null) != IntPtr.Zero)
                 {
-                    Console.WriteLine(" ^ HIT.");
-                    lhSysListView32 = DKOM.FindWindowEx(lhSysListView32, IntPtr.Zero, "SysListView32", null);
-                    lhSysHeader32 = DKOM.FindWindowEx(lhSysListView32, IntPtr.Zero, "SysHeader32", null);
+                    Console.WriteLine(" Found tasklist window at 0x" + lhSysListView32.ToString("X"));
+                    lhSysListView32 = User32.FindWindowEx(lhSysListView32, IntPtr.Zero, "SysListView32", null);
+                    lhSysHeader32 = User32.FindWindowEx(lhSysListView32, IntPtr.Zero, "SysHeader32", null);
                     break;
                 }
             }
 
-            DKOM.LockWindowUpdate(lhSysListView32);
+            User32.LockWindowUpdate(lhSysListView32);
 
             List<Process> proclist = Process.GetProcesses().ToList();
             proclist = proclist.OrderBy(x => x.ProcessName).ToList();
-            List<string> procNames = new List<string>();
 
             int index = 0;
 
             foreach (Process p in proclist)
             {
-                if(p.ProcessName.ToLower().Contains("client"))
-                    Console.WriteLine(p);
-
                 if (p.ProcessName.ToLower() == "client.vshost")
-                {
                     index = proclist.IndexOf(p);
-                    Console.WriteLine("Got");
-                }
             }
 
-            DKOM.PostMessage(lhWndParent, DKOM.WM_COMMAND, refreshNowButton, 0); //Refresh
-            DKOM.PostMessage(lhSysListView32, DKOM.LVM_SORTITEMS, 0, 0);         //Sort
-            DKOM.PostMessage(lhSysListView32, DKOM.LVM_DELETEITEM, index, 0);        //Delete
-          
-            
-            DKOM.LockWindowUpdate(IntPtr.Zero);
+
+
+            User32.PostMessage(lhWndParent, User32.WM_COMMAND, refreshNowButton, 0); //Refresh
+            User32.EnableWindow(lhSysHeader32, false);                               // Disable sorting
+            User32.PostMessage(lhSysListView32, User32.LVM_SORTITEMS, 0, 0);         //Sort
+            //DKOM.PostMessage(lhSysListView32, DKOM.LVM_DELETEITEM, index, 0);      //Delete
+            //DKOM.LockWindowUpdate(IntPtr.Zero);
 
           
             
